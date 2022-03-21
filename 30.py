5e1a54d738d0d09133016a3085ac601b3df05685
邓珊珊07 <2904033530@qq.com>
2022-03-21 06:11:07 +0000

    组合数据类型 重复元素判定续。利用集合的无重复性改编上一个程序，获得一个更快更简洁的版本。

---
+++ 30.py
@@ -0,0 +1,18 @@
+def repeat(txt):
+    ls = eval(txt)
+    st = set(ls)
+    if len(ls) != len(st):
+        return True
+def main():
+    txt = input('请输入一个列表：')
+    print(repeat(txt))
+main()
+def repeat(txt):
+    ls = eval(txt)
+    st = set(ls)
+    if len(ls) != len(st):
+        return True
+def main():
+    txt = input('请输入一个列表：')
+    print(repeat(txt))
+main()
